c06190af113579c3aa5a704a896c728b06686cba
Govinda Mandal <govinda4india@gmail.com>
2024-06-13 20:50:21 +0530

    Completed nested children component

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,26 @@
   <div class="container">
     <h1> Recursive parent-children components </h1>
     <ul>
-      <li v-for="(parent, index) in parents" :key="index">
-        <label class="container">{{ parent.name }}
-          <input type="checkbox" :checked="parent.checked">
+      <li v-for="(item, index) in items" :key="index">
+        <label class="container">{{ item.name }}
+          <input type="checkbox" :checked="item.checked" @change="onChange($event, index)">
           <span class="checkmark"></span>
         </label>
-        <ul v-if="parent.children">
-          <li v-for="(child, key) in parent.children" :key="key">
-            {{ child.name }}
-            <ul v-if="child.children">
-              <li v-for="(subchild, subkey) in child.children" :key="subkey">
-                {{ subchild.name }}
-              </li>
-            </ul>
-          </li>
-        </ul>
+        <child-component v-if="item.checked" :children="item.children" />
       </li>
     </ul>
   </div>
 </template>
 <script setup>
+
 import { reactive } from 'vue';
+import ChildComponent from './components/ChildComponent.vue';
+
 import data from './data.js';
-const parents = reactive(data);
+const items = reactive(data);
+
+function onChange(evt, index) {
+  items[index].checked = evt.target.checked;
+}
+
 </script>
--- src/components/ChildComponent.vue
+++ src/components/ChildComponent.vue
@@ -1,18 +1,30 @@
 <template>
-    <ul v-if="children.length">
-
+    <ul v-if="items.length">
+        <li v-for="(item, index) in items" :key="item.id">
+            <label class="container">{{ item.name }}
+                <input type="checkbox" :checked="item.checked" @change="onChange($event, index)">
+                <span class="checkmark"></span>
+                <child-component v-if="item.checked" :children="item.children" />
+            </label>
+        </li>
     </ul>
 </template>
 
 <script setup>
 
-import { defineProps } from 'vue';
+import { defineProps, reactive } from 'vue';
 
 const props = defineProps({
-  children: {
-    type: Array,
-    default: () => []
-  }
+    children: {
+        type: Array,
+        default: () => []
+    }
 });
+
+const items = reactive(props.children);
+
+function onChange(evt, index) {
+    items[index].checked = evt.target.checked;
+}
 
 </script>
